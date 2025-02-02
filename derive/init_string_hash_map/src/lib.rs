335c28f595a8c31ac5800815956d71e87e5f2cef
ppoliani <ppoliani@gmail.com>
2025-02-02 14:00:17 +0200

    chore: add comments

--- derive/init_string_hash_map/src/lib.rs
+++ derive/init_string_hash_map/src/lib.rs
@@ -5,6 +5,9 @@ use syn::parse_macro_input;
 use quote::ToTokens;
 use parser::InitStringHashMapParser;
 
+/// Convert structs into hash maps, that uses the String type for both keys and values.
+/// This means that it should work with any struct where all of the fields are convertible
+/// to String type using the Into trait.
 #[proc_macro_derive(IntoStringHashMap)]
 pub fn derive_into_hash_map(input: TokenStream) -> TokenStream {
   parse_macro_input!(input as InitStringHashMapParser)
