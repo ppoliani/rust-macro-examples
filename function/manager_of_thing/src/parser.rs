0d860066b9888345b61450c5184be99bf80c6923
ppoliani <ppoliani@gmail.com>
2025-02-04 14:38:26 +0200

    feat: implement parse for ManagerOfThing

--- function/manager_of_thing/src/parser.rs
+++ function/manager_of_thing/src/parser.rs
@@ -1,6 +1,6 @@
 use quote::{quote, ToTokens};
 use proc_macro2::TokenStream;
-use syn::{parse::{Parse, ParseStream}, Type, Ident};
+use syn::{parse::{Parse, ParseStream}, punctuated::Punctuated, token::Comma, GenericArgument, Ident, Token, Type, WhereClause};
 use macro_utils::type_ext::TypeExt;
 
 /// 
@@ -25,13 +25,40 @@ use macro_utils::type_ext::TypeExt;
 pub struct ManagerOfThing {
   manager_name_ident: Ident,
   manager_ty: Type,
+  manager_generic_args: Option<Punctuated<GenericArgument, Comma>>,
+  where_clause: Option<WhereClause>,
+  thing_ty: Type,
 }
 
 impl Parse for ManagerOfThing {
   fn parse(input: ParseStream) -> syn::Result<Self> {
     let manager_ty = input.parse::<Type>()?;
-    let manager_name_ident = manager_ty.get_ident();
-    todo!()
+    let manager_generic_args = manager_ty.get_angle_bracketed_generic_args();
+    
+    // Optional where clause, eg: `where K: Send+Sync+'static, V: Send+Sync+'static`.
+    let mut where_clause: Option<WhereClause> = None;
+    if input.peek(Token![where]) {
+      where_clause = Some(input.parse::<WhereClause>()?);
+    }
+
+    if let Some(idents) = &manager_ty.get_angle_bracketed_generic_args_idents() {
+      let _where = quote! {
+        where #(#idents: Send + Sync + 'static)*;
+      };
+      where_clause = Some(syn::parse(_where.into()).unwrap());
+    }
+
+    input.parse::<Token![for]>()?;
+    let thing_ty = input.parse::<Type>()?;
+    let manager_name_ident = thing_ty.get_ident().expect("Expected Type::Path::TypePath.segments to have an Ident");
+
+    Ok(ManagerOfThing {
+      manager_name_ident,
+      manager_ty,
+      manager_generic_args,
+      where_clause,
+      thing_ty,
+    })
   }
 }
 
--- macro-utils/src/type_ext.rs
+++ macro-utils/src/type_ext.rs
@@ -5,6 +5,7 @@ pub trait TypeExt {
   fn get_ident(&self) -> Option<Ident>;
   fn has_angle_bracketed_generic_args(&self) -> bool;
   fn get_angle_bracketed_generic_args(&self) -> Option<Punctuated<GenericArgument, Comma>>;
+  fn get_angle_bracketed_generic_args_idents(&self) -> Option<Vec<Ident>>;
 }
 
 impl TypeExt for Type {
@@ -21,9 +22,7 @@ impl TypeExt for Type {
       return None
     };
 
-    type_path.path.segments
-    .first()
-    .map(|s| s.ident.clone())
+    type_path.path.get_ident().map(|i| i.clone())
   }
   
   fn has_angle_bracketed_generic_args(&self) -> bool {
@@ -50,5 +49,27 @@ impl TypeExt for Type {
     };
 
     Some(angle_bracketed.args.clone())
+  }
+
+  fn get_angle_bracketed_generic_args_idents(&self) -> Option<Vec<Ident>> {
+    let Some(generic_args) = self.get_angle_bracketed_generic_args() else {
+      return None
+    };
+
+    let mut idents = Vec::new();
+
+    for generic in generic_args {
+      let GenericArgument::Type(generic_ty) = generic else {
+        continue;
+      };
+
+      let Some(ident) = generic_ty.get_ident() else {
+        continue;
+      };
+
+      idents.push(ident);
+    }
+
+    Some(idents)
   }
 }
